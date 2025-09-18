32d95138975b5eaa2ba4824cc7c962802ed530ee
OliviaDuLe <o.dustetlesk@student.uw.edu.pl>
2025-09-18 11:30:15 +0200

    Update pl.js

--- pl.js
+++ pl.js
@@ -1,9 +1,10 @@
 exetLexicon = {
-  "id": "Lufz-pl-v0.01",
-  "language": "pl",
-  "script": "Latin",
-  "letters": ["A","Ą","B","C","Ć"],
-  "lexicon": ["a","ab","ala","auto"],
-  "index": {},
-  "phindex": []
-};
+  id: "Lufz-pl-v0.01",
+  language: "pl",
+  script: "Latin",
+  letters: ["A","Ą","B","C","Ć","D","E","Ę","F","G","H","I","J","K","L","Ł","M","N","Ń","O","Ó","P","Q","R","S","Ś","T","U","V","W","X","Y","Z","Ź","Ż"],
+  lexicon: ["a", "aby", "ale", "albo", "auto", "będę", "być", "dom", "dzień", "gdzie", "jak", "jest", "koń", "lubić", "ma", "mieć", "może", "nie", "oko", "raz", "są", "tak", "ten", "to", "z"],
+  index: {},   // do wygenerowania skryptem
+  phones: [],
+  phindex: []
+}
